5eb0407dc111f676494c85691f1560295cee3b5e
Alex HM <alex13pks@gmail.com>
2023-10-24 19:52:28 -0600

    Finished Meals app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import axios from "axios";
+import LetterSelector from "./components/LetterSelector";
+
 function App() {
+  // TODO: add react router
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = (letter = "a") => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+      .then((res) => {
+        if (res.data.meals) {
+          setMeals(res.data.meals);
+        } else {
+          setMeals([]);
+        }
+      });
+  };
+
+  useEffect(() => {
+    // Get themealdb
+    getMeals();
+  }, []);
+
   return (
     <div>
-      <h1 className="bg-gradient-to-r from-indigo-400 to-pink-400 bg-clip-text text-8xl font-extrabold text-transparent">
-        We love tailwind 💙
-      </h1>
-      <p className="p-4 pl-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-      </p>
+      <LetterSelector onSelect={(letter) => getMeals(letter)} />
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {meals.map((meal) => (
+          <Card
+            key={meal.idMeal}
+            strMeal={meal.strMeal}
+            strMealThumb={meal.strMealThumb}
+            strInstructions={meal.strInstructions}
+          />
+        ))}
+      </main>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,42 @@
+function Card({ strMeal, strMealThumb, strInstructions }) {
+  return (
+    <>
+      <div className="card  h-96 w-96 bg-primary text-primary-content shadow-xl">
+        <figure>
+          <img src={strMealThumb} alt={strMeal} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{strMeal}</h2>
+          <p>
+            {strInstructions.substring(0, 150)}...
+            <button
+              className="font-bold hover:underline"
+              onClick={() => document.getElementById("my_modal_3").showModal()}
+            >
+              view more
+            </button>
+          </p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Test</div>
+          </div>
+        </div>
+      </div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="text-lg font-bold">{strMeal}</h3>
+          <p className="py-4">{strInstructions}</p>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default Card;
